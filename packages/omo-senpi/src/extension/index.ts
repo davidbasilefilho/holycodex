47c355e61bfc9f3d6b271ec58e24189492dfdbb1
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2026-07-03 22:29:41 +0900

    feat(omo-senpi): peer-external extension build pipeline

--- packages/omo-senpi/src/extension/index.ts
+++ packages/omo-senpi/src/extension/index.ts
@@ -1,7 +1,18 @@
 import { composeOmoSenpiExtension } from "./compose"
 import type { OmoSenpiComponent } from "./types"
+import { createCommentCheckerComponent } from "../components/comment-checker"
+import { createLspComponent } from "../components/lsp"
+import { createSenpiTelemetryComponent } from "../components/telemetry"
+import { createUltraworkComponent } from "../components/ultrawork"
+import { createUlwLoopComponent } from "../components/ulw-loop"
 
-const components: OmoSenpiComponent[] = []
+const components: OmoSenpiComponent[] = [
+  createUltraworkComponent(),
+  createUlwLoopComponent(),
+  createCommentCheckerComponent(),
+  createSenpiTelemetryComponent(),
+  createLspComponent(),
+]
 
 export default composeOmoSenpiExtension(components)
 export { composeOmoSenpiExtension }
