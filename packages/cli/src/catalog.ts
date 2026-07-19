0febac01ce2161efefe4156e7ad017079304a251
David Basile Filho <davidbasilefilho@gmail.com>
2026-07-19 17:42:47 -0300

    release v0.8.0

--- packages/cli/src/catalog.ts
+++ packages/cli/src/catalog.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 import { codexSlimEditInvocation, type PackageRunner } from "./package-runner.ts";
 
-export const VERSION = "0.7.4";
+export const VERSION = "0.8.0";
 
 export const SKILLS = [
   "ast-grep",
